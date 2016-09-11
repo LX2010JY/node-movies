af3096a7639502852b32c685b9ca2ae418df2faf
LX2010JY <LX2010JY@163.com>
2016-09-11 23:57:50 +0800

    I'm sorry

---
+++ gruntfile.js
@@ -0,0 +1,15 @@
+/**
+ *暂时不用了
+*/
+module.exports = function(grunt){
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
+
+	grunt.option('force',true);
+	grunt.registerTask('default',['concurrent']);
+
+
+
+}
